ff31cf85b2824e969d3d789f46f3c69b6ac2c562
Leizaa <yudiajalah2@gmail.com>
2020-04-10 20:21:33 +0700

    'test-again'

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const proc = require('./process');
+// const proc = require('./process');
 const port = 6969;
 const cors = require('cors');
 
@@ -10,7 +10,7 @@ app.all('/', function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
  });
-app.set('json spacer', 2);
+// app.set('json spacer', 2);
 
 app.get('/dummy', (req, res) => {
 	console.log('hit dummy');
